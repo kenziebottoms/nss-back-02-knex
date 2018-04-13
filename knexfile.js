f1586c015c2bf618be90ac0587ef329733ffc41b
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-04-13 16:36:42 -0500

    update knex config to use sandcastle db

--- knexfile.js
+++ knexfile.js
@@ -3,9 +3,18 @@
 module.exports = {
 
   development: {
-    client: 'sqlite3',
+    client: 'postgresql',
     connection: {
-      filename: './dev.sqlite3'
+      database: 'sandcastle',
+      user:     'kenzie',
+      password: 'kenzie'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
     }
   },
 
