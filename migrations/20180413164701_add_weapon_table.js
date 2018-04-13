62bb2ff23f51d1fe52ebde9cb1deb069794969fa
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-04-13 16:58:38 -0500

    join table 'armory' for weapons and heroes

--- migrations/20180413164701_add_weapon_table.js
+++ migrations/20180413164701_add_weapon_table.js
@@ -1,15 +1,23 @@
 
-exports.up = function(knex, Promise) {
-  return knex.schema
-    .createTable('weapons', t => {
-      t.increments('id').primary();
-      t.string('name').notNullable();
-      t.integer('owner_id');
-      t.foreign('owner_id').references('id').inTable('heroes');
-    });
+exports.up = function (knex, Promise) {
+  return Promise.all([
+    knex.schema
+      .createTable('weapons', t => {
+        t.increments('id').primary();
+        t.string('name').notNullable();
+      }),
+    knex.schema
+      .createTable('armory', t => {
+        t.increments('id').primary();
+        t.integer('owner_id').references('id').inTable('heroes');
+        t.integer('weapon_id').references('id').inTable('weapons');
+      })
+  ]);
 };
 
-exports.down = function(knex, Promise) {
-  return knex.schema
-    .dropTable('weapons');
+exports.down = function (knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('armory'),
+    knex.schema.dropTable('weapons')
+  ]);
 };
